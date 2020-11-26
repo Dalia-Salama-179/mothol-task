4b996dba07d89bc109bf71b19c105838953d1e62
dalia salama hashem <daliasalamaa@gmail.com>
2020-11-26 08:19:27 +0200

    value

--- src/components/AppSelect2.vue
+++ src/components/AppSelect2.vue
@@ -2,8 +2,8 @@
     <select class="select2-obj" style="width: 100%" ref="select2Obj" :disabled="disabled">
         <option></option>
         <template v-if="keys">
-            <option v-for="dept in options" :key="dept[keys[0]]" :value="dept[keys[0]]"
-                    :selected="dept[keys[0]] == value">{{dept[keys[1]]}}</option>
+            <option v-for="(dept) in options" :key="dept[keys[0]]" :value="dept[keys[0]]"
+                    :selected="dept[keys[0]] == parseInt(value)">{{dept[keys[1]]}}</option>
         </template>
         <template v-else>
             <option v-for="opt in options" :key="opt" :value="opt"
@@ -18,8 +18,20 @@
     export default {
         name: "AppSelect2",
         props: ['options', 'keys', 'disabled', 'value'],
+        watch: {
+            'value'() {
+
+                if (this.value) {
+                    $(this.$refs.select2Obj).select2("val", this.value);
+                }
+            }
+        },
         mounted() {
             let that = this;
+
+            if (this.value) {
+                $(this.$refs.select2Obj).select2("val", this.value);
+            }
             $(this.$refs.select2Obj).select2({
                 width: 'resolve',
                 allowClear: true,
